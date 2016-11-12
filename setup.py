462bc3ecc15248d5d72dc12151a2a73f8a87f1d0
Ryan Castner <castner.rr@gmail.com>
2016-11-12 14:14:01 -0500

    Add: PyPI package publishing

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django-language-field',
+  packages = ['django-language-field'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'A pluggable django app that provides a comprehensive language choices field',
+  author = 'Ryan Castner',
+  author_email = 'ryancastner@gmail.com',
+  url = 'https://github.com/audiolion/django-language-field', # use the URL to the github repo
+  download_url = 'https://github.com/audiolion/django-language-field/tarball/0.0.1', # I'll explain this in a second
+  keywords = ['django','language','languages','field'], # arbitrary keywords
+  classifiers = [],
+)

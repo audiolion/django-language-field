878dd091a53f013096c5af8f2c974c15a69e8701
Victor Porton <porton@narod.ru>
2019-06-19 12:11:27 +0300

    added caching

--- languages/forms.py
+++ languages/forms.py
@@ -1,11 +1,32 @@
 from django import forms
+from django.core.cache import caches
+
+sorted_languages = None
+def _get_sorted_languages():
+    from .languages import LANGUAGES
+    global sorted_languages
+    if sorted_languages:
+        return sorted_languages
+    sorted_languages = sorted(LANGUAGES, key=lambda p: p[1])
+    return sorted_languages
+
+class _LanguageWidget(forms.Select):
+    def render(self, name, value, attrs=None, renderer=None):
+        cache = caches['default']
+        result = cache.get(value or '')
+        if result is not None:
+            return result
+        result = super().render(name, value, attrs, renderer)
+        cache.set(value or '', result)
+        return result
 
 class LanguageField(forms.ChoiceField):
     """
     A language field for Django forms.
     """
+    widget = _LanguageWidget
+
     def __init__(self, *args, **kwargs):
-        from .languages import LANGUAGES
-        defaults = {'choices': sorted(LANGUAGES, key=lambda p: p[1])}
+        defaults = {'choices': _get_sorted_languages()}
         defaults.update(kwargs)
         super().__init__(*args, **defaults)

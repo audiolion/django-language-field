75d8e85f8e10e6e2d189cbf2e1cac8b05f2b0bd2
Richard Stromer <noxan@users.noreply.github.com>
2022-05-24 17:07:50 +0200

    Fix db_collation attribute of LanguageField (#8)
    * add missing db_collation=None to language field
    
    * pass db_collation as argument in super call
    
    * remove unnecessary arguments from super call

--- languages/fields.py
+++ languages/fields.py
@@ -4,10 +4,10 @@ class LanguageField(CharField):
     """
     A language field for Django models.
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, db_collation=None, **kwargs):
         # Local import so the languages aren't loaded unless they are needed.
         from .languages import LANGUAGES
 
         kwargs.setdefault('max_length', 3)
         kwargs.setdefault('choices', LANGUAGES)
-        super(CharField, self).__init__(*args, **kwargs)
+        super().__init__(*args, db_collation, **kwargs)
